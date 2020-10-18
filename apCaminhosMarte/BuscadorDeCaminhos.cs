830f7bf3cd661b475320e6d4258355c7ffea3b2d
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2020-10-18 16:53:59 -0300

    comentar classe BuscadorDeCaminhos

--- apCaminhosMarte/BuscadorDeCaminhos.cs
+++ apCaminhosMarte/BuscadorDeCaminhos.cs
@@ -21,35 +21,53 @@ namespace apCaminhosMarte
 
         public List<List<Caminho>> BuscarCaminho(int idOrigem, int idDestino)
         {
+            // inicializar as listas necessárias para execução
             todosCaminhos = new List<List<Caminho>>();
             jaVisitados = new List<int>();
 
+            // como estamos na cidade de origem, adicioná-la à lista de cidades visitadas
             jaVisitados.Add(idOrigem);
 
+            // chamar o método interno BuscarCaminho(), passando uma lista de Caminhos vazia para representar o conjunto de Caminhos atual
             BuscarCaminho(new List<Caminho>(), idOrigem, idDestino);
 
+            // se não há caminhos entre as cidades selecionadas, retorna null
             if (todosCaminhos.Count() == 0) return null;
 
+            // senão, retorna os caminhos
             return todosCaminhos;
         }
 
         private void BuscarCaminho(List<Caminho> caminhoAtual, int cidadeAtual, int cidadeDestino)
         {
+            // para todas as cidades que existem,
             for(int i = 0; i < Matriz.Tamanho; i++)
             {
+                // gerar um caminho entre a cidade atual e a nova cidade
                 Caminho caminhoTeste = Matriz.BuscarPeloIndice(cidadeAtual, i);
+                // se esse caminho existir e não fomos pra essa cidade nesse 'braço' da execução,
                 if (caminhoTeste != null && caminhoTeste.Distancia > 0 && !jaVisitados.Any(item => item == i))
                 {
+                    // vamos para a nova cidade, e a adicionamos ao caminho atual
                     caminhoAtual.Add(caminhoTeste);
 
+                    // se chegamos no destino do usuário, salvamos um clone do caminho atual como um dos caminhos possíveis
                     if (i == cidadeDestino)
                         todosCaminhos.Add(caminhoAtual.Select(c => c.Clone()).ToList());
                     else
                     {
+                        // senão, usamos recursão:
+                        // adicionamos a cidade atual à lista de visitadas
                         jaVisitados.Add(i);
+                        // chamamos o mesmo método, passando o caminho atual e começando da cidade em que estamos
                         BuscarCaminho(caminhoAtual, i, cidadeDestino);
+                        /* ao finalizar o método, significa que todas as cidades de 'níveis' mais distantes do que essa
+                         * ja foram verificadas, então podemos removê-la da lista de cidades visitadas, para que outras
+                         * iterações do backtracking possam passar por ela
+                         */
                         jaVisitados.Remove(i);
                     }
+                    // no fim, temos que remover a ultima etapa do caminho atual, seguindo a lógica de backtracking
                     caminhoAtual.RemoveAt(caminhoAtual.Count - 1);
                 }
             }
